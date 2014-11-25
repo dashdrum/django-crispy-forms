a37e94af9ea29fdc6514fbd0f32f9c3f1eefccce
dashdrum <dan@dashdrum.com>
2014-11-24 19:35:39 -0500

    Update unicode literals for Python 3.2 compatibility

--- crispy_forms/tests/test_layout.py
+++ crispy_forms/tests/test_layout.py
@@ -60,7 +60,7 @@ class TestFormLayout(CrispyTestCase):
                 self.fields['contraseña'] = forms.CharField()
 
             helper = FormHelper()
-            helper.layout = Layout(u'contraseña')
+            helper.layout = Layout('contraseña')
 
         if PY2:
             self.assertRaises(Exception, lambda: render_crispy_form(UnicodeForm()))
@@ -149,19 +149,19 @@ class TestFormLayout(CrispyTestCase):
         form_helper.add_layout(
             Layout(
                 Fieldset(
-                    u'Company Data',
-                    u'is_company',
+                    'Company Data',
+                    'is_company',
                     css_id = "fieldset_company_data",
                     css_class = "fieldsets",
                     title = "fieldset_title",
                     test_fieldset = "123"
                 ),
                 Fieldset(
-                    u'User Data',
-                    u'email',
+                    'User Data',
+                    'email',
                     Row(
-                        u'password1',
-                        u'password2',
+                        'password1',
+                        'password2',
                         css_id = "row_passwords",
                         css_class = "rows",
                     ),
@@ -169,8 +169,8 @@ class TestFormLayout(CrispyTestCase):
                     HTML("""
                         {% if flag %}{{ message }}{% endif %}
                     """),
-                    u'first_name',
-                    u'last_name',
+                    'first_name',
+                    'last_name',
                 )
             )
         )
@@ -318,7 +318,7 @@ class TestFormLayout(CrispyTestCase):
         form_helper.add_layout(
             Layout(
                 Fieldset(
-                    u'Company Data',
+                    'Company Data',
                     'is_company',
                     'email',
                     'password1',
